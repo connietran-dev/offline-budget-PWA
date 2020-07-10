312dfc03a70558f4f4880ca29cad8f2c78dc9cc6
Connie Tran <connietrandev@gmail.com>
2020-07-10 16:28:53 -0400

    Generate manifest with webpack-pwa-manifest plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,47 @@
-console.log("directory" + __dirname + "/public/dist");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 const config = {
-    entry: "./public/index.js",
-    output: {
-      path: __dirname + "/public/dist",
-      filename: "bundle.js"
-    },
-    mode: "development"
-  };
-  
-  module.exports = config;
+  entry: "./public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  plugins: [
+    new WebpackPwaManifest({
+      // the name of the generated manifest file
+      filename: "manifest.json",
+
+      // we aren't using webpack to generate our html so we
+      // set inject to false
+      inject: false,
+
+      // set fingerprints to `false` to make the names of the generated
+      // files predictable making it easier to refer to them in our code
+      fingerprints: false,
+
+      name: "IndexBudget Tracker PWA",
+      short_name: "Index Budget",
+      theme_color: "#e6f542",
+      background_color: "#e6f542",
+      start_url: "/",
+      display: "fullscreen",
+      "orientation": "landscape",
+
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            "public/icons/icon-512x512.png"
+          ),
+          // the plugin will generate an image for each size
+          // included in the size array
+          size: [72, 96, 128, 144, 152, 192, 384, 512]
+        }
+      ]
+    })
+  ]
+};
+
+module.exports = config;
