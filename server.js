d48415739a2b812912caf18d30af26c0993dd4c3
Connie Tran <connietrandev@gmail.com>
2020-07-11 09:41:57 -0400

    Add PROCESS.ENV for Heroku MongoDB

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(PROCESS.ENV.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
